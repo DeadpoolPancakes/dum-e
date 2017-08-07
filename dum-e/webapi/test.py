681d520ba7bcb5ab2e1c6d5098f579bcd9e490a7
Jamie Naylor <jamie@naylorwebsites.co.uk>
2017-08-07 12:15:57 +0100

    playing around with file reading

--- dum-e/webapi/test.py
+++ dum-e/webapi/test.py
@@ -1,8 +1,10 @@
-file = open("testfile.txt","w") 
+file = open("testfile.txt","r")
  
-file.write("Hello World \n") 
-file.write("This is our new text file \n") 
-file.write("and this is another line. \n") 
-file.write("Why? Because we can.") 
- 
-file.close() 
+print ("booya")
+
+v = 0
+for i in file.readlines():
+    v = v + 1
+    print(v)
+    print (i)
+  

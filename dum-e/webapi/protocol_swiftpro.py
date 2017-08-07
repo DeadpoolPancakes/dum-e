b497a9aa00195856e9aae564b82718e61eb8d89f
Jamie Naylor <jamie@naylorwebsites.co.uk>
2017-08-07 11:14:39 +0100

    Added more gcodes to the protocol

--- dum-e/webapi/protocol_swiftpro.py
+++ dum-e/webapi/protocol_swiftpro.py
@@ -1,7 +1,7 @@
 # uArm Swift Pro - Python Library - protocol commands
 # Created by: Richard Garsthagen - the.anykey@gmail.com
 # V0.1 - June 2017 - Still under development
-
+# v.05 august 2017 - deadpoolpancakes
 
 ## PROTOCOL MESSAGE
 READY                   = "@1"
@@ -37,3 +37,25 @@ SET_EEPROM              = "M2212 N0 A{} T{} V{}"
 GET_ANALOG              = "P2241 N{}"
 GET_DIGITAL             = "P2240 N{}"
 SET_BT                  = "M2234 V{}"
+
+# grove modules
+SET_GROVE               = "M2300 N{}"
+GET_COLOR               = "M2301 N10 V{}"
+#@10 N10 R20 G10 B255\n
+GET_GESTURE             = "M2301 N11 V{}"
+#@10 N11 V16\n (1: right; 2: left; 4: up; 8: down; 16: forward; 32: backward; 64: CW; 128: CCW;)
+GET_ULTRASONIC          = "M2303 N12 V{}"
+#@10 N12 V27\n
+GET_TEMP                = "M2303 N15 V{}"
+#@10 N15 T32.12 H76.5\n
+GET_MOTION              = "M2303 N16 V{}"
+#@10 N16 V1\n
+SET_FAN                 = "M2303 N13 V{}"
+# fan speed 0 - 255
+SET_MAGNET              = "M2302 N14 V{}"
+
+##LCD DISPLAY
+SET_DISPLAY             = "M2303 N17 T{}"
+SET_DISPLAY_RGB         = "M2303 N17 R{} G{} B{}"
+SET_DISPLAY_TEXT        = "M2303 S{} V{}"
+#S IS LINE AND V IS TEXT TO DISPLAY
